9a047bc7bc489159b21b6e0a9b9d55a83c37d5d6
iftekharul152811 <adnansarker792@gmai.com>
2022-11-10 00:18:07 +0600

    delete my review api done

--- index.js
+++ index.js
@@ -51,7 +51,34 @@ async function run() {
             res.send(result);
         })
 
+        // get review 
+        app.get('/reviews', async (req, res) => {
+            const query = {};
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        })
 
+        //get my-review
+        app.get('/myReviews', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        })
+
+        // delete my review
+        app.delete('/myReviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
 
     }
     finally {
