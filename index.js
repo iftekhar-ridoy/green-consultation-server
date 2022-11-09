c0aecb9e87e5465ad5a27698880bd349ed5eb42b
iftekharul152811 <adnansarker792@gmai.com>
2022-11-09 11:45:07 +0600

    posting review api done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 // middleWare 
@@ -17,7 +17,9 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('greenConsult').collection('services');
+        const reviewCollection = client.db('greenConsult').collection('reviews');
 
+        // get maximum 3 service 
         app.get('/', async (req, res) => {
             const query = {};
             const size = 3;
@@ -25,12 +27,32 @@ async function run() {
             const services = await cursor.limit(size).toArray();
             res.send(services);
         })
+
+        // get all services 
         app.get('/services', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         })
+
+        // get individual service details 
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+
+        // post review
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+        })
+
+
+
     }
     finally {
 
