fcbb80f1d361ab5dd64375349d8bc201a9db86fe
iftekharul152811 <adnansarker792@gmai.com>
2022-11-10 15:19:35 +0600

    dotenv config added which i missed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 
 
 // middleWare 
@@ -10,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = 'mongodb+srv://greenConsult:otDJMFWb89g2DAcT@cluster0.qj1nhmw.mongodb.net/?retryWrites=true&w=majority';
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qj1nhmw.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
